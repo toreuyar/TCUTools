dbf314b088f8a6d2ab5f0ca6acfbf01775e852b4
Töre Çağrı Uyar <mail@toreuyar.net>
2015-04-21 08:15:51 +0300

    Updated Example project.

--- Example/Pods/Target Support Files/Pods-Tests-TCUTools/Pods-Tests-TCUTools-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_TCUTools : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_TCUTools
-@end
---
+++ Example/Tests/TCUTypeSafeCollectionTests.m
@@ -0,0 +1,33 @@
+//
+//  TCUTypeSafeCollectionTests.m
+//  TCUTools
+//
+//  Created by Töre Çağrı Uyar on 21/04/15.
+//  Copyright (c) 2015 To&#776;re C&#807;ag&#774;r&#305; Uyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TCUTypeSafeCollectionTests : XCTestCase
+
+@end
+
+@implementation TCUTypeSafeCollectionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+@end
